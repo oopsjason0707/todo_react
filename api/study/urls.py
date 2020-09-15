67d1ce5ec5bcf05486a00e7b928685bd4a678162
Jason <jasonoh1491@gmail.com>
2020-09-15 13:20:28 +0900

    CBV, FBV 방식 연습

--- api/study/urls.py
+++ api/study/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
     path('students/', views.StudentView),
-    path('scores/', views.ScoreView)
+    path('students/<id>', views.StudentDetailView),
+    path('Scores/<id>', views.ScoreDetailView),
+    path('scores/', views.ScoreView),
+    path('scores', views.ScoreView.as_view)
 ]
 
  
